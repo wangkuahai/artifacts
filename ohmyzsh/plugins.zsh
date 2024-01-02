263c5f4ca519f1307636fc31dadfce4fd4d00553
Yueyang Wang <43435685+wangkuahai@users.noreply.github.com>
2024-01-02 16:47:06 +0800

    Update plugins.zsh

--- ohmyzsh/plugins.zsh
+++ ohmyzsh/plugins.zsh
@@ -4,7 +4,7 @@ ZSH_CUSTOM=~/.oh-my-zsh/custom
 cp --backup=numbered ~/.zshrc ~/.zshrc-copy
 
 # zsh-autosuggestions
-git clone git@github.com:zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com:zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 sed -i -E 's/(plugins[[:space:]]*=[[:space:]]*\()/\0zsh-autosuggestions /' ~/.zshrc
 
 # wget http://mimosa-pudica.net/src/incr-0.2.zsh
